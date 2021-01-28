e05efa0395328c05c15595a89307a15c2ff05d39
Mert18 <mertuygur02@gmail.com>
2021-01-28 11:00:27 +0300

    some changes

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const movieButton = document.querySelector("#movie");
 const bookButton = document.querySelector("#book");
 
 movieButton.addEventListener('click', () => {
-    alert('hello')
+    movieButton.classList.add("active");
+    bookButton.classList.remove("active");
+})
+bookButton.addEventListener('click', () => {
+    bookButton.classList.add("active");
+    movieButton.classList.remove("active");
 })
