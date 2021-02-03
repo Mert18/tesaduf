393d61d895841ff6d2202b2268a39e03c2e604f7
Mert <mertuygur02@gmail.com>
2021-02-03 18:24:55 +0300

    bg, github

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import '../index.css'
 const Header = () => {
     return (
         <div className="header">
+            <a href="https://github.com/Mert18/random-is-beautiful">GITHUB</a>
             <h1>random is beautiful!</h1>
         </div>
     )
